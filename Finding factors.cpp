1be391fbe1a202a2fe42c928bc3f06f1b08f4444
Shobha-27 <88886122+Shobha-27@users.noreply.github.com>
2021-10-28 10:50:16 +0530

    Create Finding factors.cpp

---
+++ Finding factors.cpp
@@ -0,0 +1,27 @@
+//Finding  Factors
+
+/*The program takes an integer as input and print out its no. of factors and all the factors*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    long long int n;vector<int> v;
+
+    cin >> n;
+
+    for(long long int i = 1;i<=n;i++){if(n%i==0){v.push_back(i);}}
+
+    cout << v.size() << "\n";
+
+    for(vector<int>::iterator iter = v.begin();iter != v.end();++iter){
+
+        cout << *iter << " ";
+
+    }
+
+    return 0;
+
+}
